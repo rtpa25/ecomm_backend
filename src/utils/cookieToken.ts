5363ed3b50efca3aec7fe098f812b7c02fe75295
Ronit Panda <pandaronit25@gmail.com>
2021-12-03 19:50:35 +0530

    test

--- src/utils/cookieToken.ts
+++ src/utils/cookieToken.ts
@@ -10,7 +10,6 @@ export const cookieToken = (user: UserDocument, res: Response) => {
     .status(200)
     .cookie('token', token, {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
     })
     .json({
       success: true,
