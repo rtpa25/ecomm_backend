8d049e448150d7cd9ea83a53c822633e1670bb5d
Ronit Panda <pandaronit25@gmail.com>
2021-12-03 19:27:49 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
 app.use(express.json({ limit: '50MB' }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://ecomm-app-theta.vercel.app/',
     credentials: true,
     // allowedHeaders: ['Access-Control-Allow-Origin', '*'],
   })
