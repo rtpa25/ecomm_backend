a823b8452643c94209d8bb9b97ac83fca4c41c81
Ronit Panda <pandaronit25@gmail.com>
2021-12-03 15:45:19 +0530

    f

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
 app.use(express.json({ limit: '50MB' }));
 app.use(
   cors({
-    origin: 'https://ecomm-app-theta.vercel.app/',
+    origin: 'https://ecomm-app-theta.vercel.app',
     credentials: true,
   })
 );
